572bce32e4b2b1bf7de7a26e5049af8c064cc6a0
aarslantas <a_arslantas69@hotmail.com>
2025-06-18 11:39:54 -0700

    add screens for AllExpenses, ManageExpense, and RecentExpenses components

---
+++ screens/AllExpenses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const AllExpenses = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      abdurrahman
+    </View>
+  );
+};
+
+export default AllExpenses;
---
+++ screens/RecentExponses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const RecentExpenses = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      abdurrahman
+    </View>
+  );
+};
+
+export default RecentExpenses;
