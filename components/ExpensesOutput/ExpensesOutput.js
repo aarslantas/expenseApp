500ae2176dd371e0450c3366cb5ac71f11d550b5
aarslantas <a_arslantas69@hotmail.com>
2025-06-22 16:06:01 +0300

    feat: Implement expenses summary and display in All Expenses screen

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -2,10 +2,34 @@ import { View, Text, FlatList } from 'react-native';
 import ExpensesSummary from './ExpensesSummary';
 import ExpensesList from './ExpensesList';
 
-function ExpensesOutput({ expenses }) {
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    description: 'Groceries',
+    amount: 50.0,
+    date: new Date('2023-10-01'),
+  },
+  {
+    id: 'e2',
+    description: 'Rent',
+    amount: 1200.0,
+    date: new Date('2023-10-02'),
+  },
+  {
+    id: 'e3',
+    description: 'Utilities',
+    amount: 200.0,
+    date: new Date('2023-10-03'),
+  },
+];
+
+function ExpensesOutput({ expenses, expensesPeriod, fallbackText }) {
   return (
     <View>
-      <ExpensesSummary />
+      <ExpensesSummary
+        expenses={DUMMY_EXPENSES}
+        expensesPeriod={expensesPeriod}
+      />
       <ExpensesList />
     </View>
   );
