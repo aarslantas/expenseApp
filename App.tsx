c4f1d9a0fb23e83565be0dd7f60e25b9b48c78f9
aarslantas <a_arslantas69@hotmail.com>
2025-07-05 21:41:14 +0300

    fix: Update tabBarLabel for AllExpenses screen to avoid duplication

--- App.tsx
+++ App.tsx
@@ -69,7 +69,7 @@ function ExpensesOverview() {
         component={AllExpenses}
         options={{
           title: 'Recent Expenses',
-          tabBarLabel: 'Recent',
+          tabBarLabel: 'Recent1',
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="Home" size={size} color={color} />
           ),
