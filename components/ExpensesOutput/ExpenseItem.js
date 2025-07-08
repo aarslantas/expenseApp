6207bae018aadcdb316976475a22164c2fa5f7ca
aarslantas <a_arslantas69@hotmail.com>
2025-07-08 22:20:35 +0300

    fix: Pass expense details to ManageExpense screen for editing

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -4,11 +4,16 @@ import { Pressable, Text, View, StyleSheet } from 'react-native';
 import { GlobalStyles } from '../../constants/style';
 import { getFormattedDate } from '../../util/date';
 
-function ExpenseItem({ description, amount, date }) {
+function ExpenseItem({ id, description, amount, date }) {
   const navigation = useNavigation();
 
   function handleExpressItem() {
-    navigation.navigate('ManageExpense');
+    navigation.navigate('ManageExpense', {
+      expenseId: id,
+      description: description,
+      amount: amount,
+      date: date,
+    });
     console.log('Expense Item Pressed:', description, amount, date);
   }
 
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { Text, View } from 'react-native';
 
-const ManageExpense = () => {
+const ManageExpense = ({ route, navigation }) => {
+  const editedExpenseId = route.params?.expenseId;
+
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: isEditing ? 'Edit Expense' : 'Add Expense',
+    });
+  }, [navigation, isEditing]);
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>abdurrahman</Text>
