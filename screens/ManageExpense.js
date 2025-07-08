8e736577c17fd3e5772cf6dcbd4251d2230cdf2b
aarslantas <a_arslantas69@hotmail.com>
2025-07-08 22:55:20 +0300

    fix: Implement delete functionality in ManageExpense screen

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,8 @@
 import React, { useLayoutEffect } from 'react';
 import { Text, View } from 'react-native';
+import IconButton from '../components/ui/IconButton';
+import { GlobalStyles } from '../constants/styles';
+import { StyleSheet } from 'react-native/types_generated/index';
 
 const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
@@ -12,11 +15,41 @@ const ManageExpense = ({ route, navigation }) => {
     });
   }, [navigation, isEditing]);
 
+  const handleDeleteExpense = () => {
+    // Logic to delete the expense goes here
+    console.log('Expense deleted');
+    navigation.goBack();
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>abdurrahman</Text>
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={handleDeleteExpense}
+          />
+        </View>
+      )}
     </View>
   );
 };
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+});
